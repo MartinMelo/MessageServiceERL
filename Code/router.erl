e1dd502d669d0a750310249f9ec2ef2dd6c3b13c
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2016-07-01 22:57:13 -0300

    Modulo router terminado.

--- Code/router.erl
+++ Code/router.erl
@@ -1,4 +1,29 @@
 -module(router).
--export([]).
+-export([start/0]).
 
-%%Definition pending
+start()->
+  spawn(fun()-> init() end).
+
+init()->
+  loop([]).
+
+loop(Servers)->
+  receive
+    {register, Server}->
+      loop([Server |Servers]);%Lo meto en la lista.
+    {unregister, Server}->
+      ServersUpdated = quitarServer(Servers, Server),
+      loop(ServersUpdated);
+    {request , Who}->
+      Server = getRandomServer(Servers),
+      Who ! {server , Server},
+      loop(Servers)
+  end.
+
+%Elimina el server de la lista.
+quitarServer(Servers, Server)->
+  lists:keydelete(Server,1,Servers).%Key,Cuantos,Lista.
+
+%Devuelve un server de la lista de forma random.
+getRandomServer(Servers)->
+  lists:nth(random:uniform(length(Servers)), Servers).
