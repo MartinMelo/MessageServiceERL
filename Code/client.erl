b788be3985601e514ed0617ec5083d5f5b7e5ff6
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2016-07-02 00:06:34 -0300

    Un poco de logica del cliente.

--- Code/client.erl
+++ Code/client.erl
@@ -1,10 +1,39 @@
 -module(client).
--export([start/1, start/2]).
+-export([start/1]).
 
 
-start(host)->
-  asd.
+start(Router)->
+  spawn(fun()-> init(Router) end).
 
-start(host,clientName)->
-  asd.
+init(Router)->
+  io:format("Conectando a Router ~n~p", [Router]),
+  enviarPeticionARouter(Router),
+  receive
+    {server, Server}-> loop(Router, Server)
+  end.
 
+
+%Pido al router que me de un server.
+enviarPeticionARouter(Router)->
+  Router ! {request, self()}.
+
+loop(Router, Server)->
+  receive
+    {emit, Msg}->
+      Server ! {emit, Msg , self()},
+      loop(Router, Server);
+    {subscribe, Channel}->
+      Server ! {subscribe, Channel , self()},
+      loop(Router, Server);
+    {desubscribe, Channel}->
+      Server ! {desubscribe, Channel , self()},
+      loop(Router, Server);
+    {msg, Msg}->
+      procesarMensajeYEnviarACK(Msg, Server),
+      loop(Router, Server)
+  end.
+
+procesarMensajeYEnviarACK(Msg , Server)->
+  %Imprimir informacion del mensaje,
+  IdMsg = asd,
+  Server ! {ack, IdMsg}.
