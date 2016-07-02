cc3925a5736bca7b6ae0659c2a7645c4caa33254
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2016-07-02 14:59:36 -0300

    Parece que el sender esta completo.

--- Code/sender.erl
+++ Code/sender.erl
@@ -1,3 +1,17 @@
 -module(sender).
--export([]).
+-export([start/0]).
 %Cada Server tendra una instancia de su propio sender, el cual realiza los envios de mensajes.
+
+start()->
+  spawn(fun()-> init() end ).
+
+init()->
+  loop().
+
+%Recibe mensajes para enviar y los publica de una.
+loop()->
+  receive
+    {send, {Msg, Client}}->
+      Client ! Msg
+  end.
+
