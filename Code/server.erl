cd515d94be764e226dfb0024c87aa53edeb45415
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2016-07-06 03:25:24 -0300

    No hace falta el subscribe en todos los servidores. simplemente el emit envia a todos los servers. Agregue varios io:format

--- Code/server.erl
+++ Code/server.erl
@@ -13,45 +13,50 @@ init(Suscripciones, Servers, Sender)->
 loop(Suscripciones, Servers, Sender)->
   receive
     {register, Router} ->
-      io:format("Registrandome..."),
+      io:format("Registering~n"),
       Router ! {register, self()},
-      io:format("Registro exitoso!"),
-
       loop(Suscripciones, Servers, Sender);
     {brothers, ListaDeHermanos}->
+      io:format("Brother List: ~p~n", [ListaDeHermanos]),
       loop(Suscripciones, ListaDeHermanos, Sender);
     {addBrother, Server}->
+      io:format("Brother added: ~p~n", [Server]),
       loop(Suscripciones, [Server | Servers], Sender);
     {removeBrother, Server}->
+      io:format("Brother removed: ~p~n", [Server]),
       ListaNueva = removerServer(Server, Servers),
       loop(Suscripciones, ListaNueva, Sender);
     {subscribe, {Channel, Client}}->
+      io:format("Client subscripted: ~p~n", [Client]),
       NuevasSuscripciones = suscribir(Suscripciones, Channel, Client,Servers),
       loop(NuevasSuscripciones, Servers, Sender);
     {unsubscribe, {Channel, Client}}->
+      io:format("Client unsubscripted: ~p~n", [Client]),
       NuevasSuscripciones = desuscribir(Suscripciones, Channel, Client,Servers),
       loop(NuevasSuscripciones, Servers, Sender);
     {emit, {Channel, Client, Message}}->
+      io:format("Emitiendo ~n"),
       emitir(Channel, Suscripciones, Client, Message, Sender,Servers),
-      io:format("Mensaje emitido del server"),
       loop(Suscripciones, Servers, Sender);
     {emit, {Channel, Client, Message, imServer}}->
+      io:format("Emit recibed from another server ~n"),
       emitir(Channel, Suscripciones, Client, Message, Sender),
       loop(Suscripciones, Servers, Sender);
     state ->
+      io:format("State ~n"),
       imprimirEstado(Suscripciones, Servers)
   end.
 
 imprimirEstado(Suscripciones, Servers)->
-  io:format("Servers disponibles: ~n", Servers),
-  io:format("Suscripciones disponibles: ~n", Suscripciones).
+  io:format("Servers disponibles: ~p~n", Servers),
+  io:format("Suscripciones disponibles: ~p~n", dict:to_list(Suscripciones)).
+
 %Remueve el server de la lista de servers.
 removerServer(Server, Servers)->
   lists:keydelete(Server, 1, Servers).
 
 %Suscribe el cliente al channel deseado.
 suscribir(Suscripciones, Channel, Client, Servers)->
-  suscribirEnTodosLosServers(Channel, Client,Servers),
   %Buscar si existe la suscripcion en la lista.
   %Si existe agregarlo en la lista y si no crear el channel y ponerla.
   case dict:find(Channel, Suscripciones) of
@@ -69,21 +74,14 @@ suscribir(Suscripciones, Channel, Client, Servers)->
         dict:append(Channel, [Client], Suscripciones)
   end.
 
-suscribirEnTodosLosServers(Channel, Client,Servers)->
-  lists:map(fun(Server)-> Server ! {subscribe, {Channel, Client}} end, Servers).
-
 %desuscribe el cliente del channel deseado.
 desuscribir(Suscripciones, Channel, Client, Servers)->
-  desuscribirEnTodosLosServers(Channel, Client,Servers),
   %Buscar el channel en la lista y borrar el client del channel.
   case dict:find(Channel, Suscripciones) of
     {ok, SuscripcionesDelCanal} ->
       lists:keydelete(Client,1,SuscripcionesDelCanal);
     error->Suscripciones
   end.
-
-desuscribirEnTodosLosServers(Channel, Client,Servers)->
-  lists:map(fun(Server)-> Server ! {unsubscribe, {Channel, Client}} end, Servers).
 
 emitir(Channel, Suscripciones, Client, Message, Sender, Servers)->
   emitirAServers(Channel, Client, Message, Sender, Servers),
