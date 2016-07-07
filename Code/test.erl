171b16c9d3722ec79cd0c105ba647c2652a23f48
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2016-07-06 23:15:00 -0300

    Test actualizado

--- Code/test.erl
+++ Code/test.erl
@@ -2,30 +2,33 @@
 -export([start/0]).
 
 start()->
-  Servers = crearServidores(),
-  Router = router:start(),
-  agregarServidoresAlRouter(Servers, Router),
-  Clientes =crearClientes(Router),
+  Clientes =crearClientes({router,'router@192.168.0.105'},[],0),
   correrTest(Clientes).
 
-
-agregarServidoresAlRouter(Servers, Router)->
-  lists:foreach(fun(Server)-> Router ! {register, Server} end, Servers).
-
-crearClientes(Router)->
-  [client:start(Router),client:start(Router),client:start(Router)].
-
-%Crea 4 Servers.
-crearServidores() ->
-  [server:start(),server:start(),server:start(),server:start()].
+crearClientes(_,Clientes, 1000)-> Clientes;
+crearClientes(Router,Clientes,N)->
+  crearClientes(Router,[client:start(Router) | Clientes], N+1).
 
 %%%%%%%%%%%%%%%%%%%%LOGICA DEL TEST%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 correrTest(Clientes)->
-  lists:foreach(fun(Cliente)-> Cliente ! {subscribe , "#"} end, Clientes),
-  Cliente = lists:nth(1, Clientes),
-  Cliente ! {emit, "OLA K ASE! BO!"}.
+  Subs = ["chat","rp", "zaza","BO","st","multi","toty","muty","routy","detector","namy","loggy","groupy","crash","opty","rudy"],
+  suscribirATodos(Clientes, Subs),
+  io:format("esperando 5 segundos"),
+  timer:sleep(random:uniform(5000)),
+  loopear(Clientes,0, Subs).
 
 
 
+suscribirATodos(Clientes, Subs)->
+  Sub = lists:nth(random:uniform(length(Subs)), Subs),
+  lists:foreach(fun(Cliente)-> Cliente ! {subscribe , Sub} end, Clientes).
 
 
+loopear(_ , 100, _)->
+  finished;
+loopear(Clientes, N, Subs)->
+  Cliente = lists:nth(random:uniform(length(Clientes)), Clientes),
+  Sub = lists:nth(random:uniform(length(Subs)), Subs),
+  Cliente ! {emit, Sub ,Sub},
+  timer:sleep(random:uniform(1000)),
+  loopear(Clientes,N+1,Subs).
